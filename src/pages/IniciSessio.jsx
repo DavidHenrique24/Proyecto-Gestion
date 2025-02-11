dcbdea0aa42f467e81def48856de4075704ef7b6
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-02-11 19:24:23 +0100

    Integracion de Bootstrap

--- src/pages/IniciSessio.jsx
+++ src/pages/IniciSessio.jsx
@@ -5,8 +5,22 @@ const IniciSessio = () => {
   return (
     <div>
       <h1>Inicio de sesion </h1>
-     
+      {/* Texto de prueba  */}
+      <form>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Correo electrónico</label>
+          <input type="email" className="form-control" id="email" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Contraseña</label>
+          <input type="password" className="form-control" id="password" />
+        </div>
+        <button type="submit" className="btn btn-primary">Iniciar sesión</button>
+      </form>
     </div>
+      
+     
+
   );
 };
 
