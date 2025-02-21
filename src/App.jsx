3663ee2851c162a36a333a9ab2b4cff2c491a2c6
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-02-21 16:12:28 +0100

    Creació de col·leccions a localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ import Registre from './pages/Registre';
 import Panell from './pages/Panell';
 import Header from './componentes/Header';
 
+// Inicializa la colección de tiquets si no existe con Localstoreage
+if (!localStorage.getItem('dades_tiquets')) {
+  localStorage.setItem('dades_tiquets', JSON.stringify([]));
+}
+
+// Inicializa la colección de usuarios si no existe
+if (!localStorage.getItem('dades_usuaris')) {
+  localStorage.setItem('dades_usuaris', JSON.stringify([])); //JSON.stringify([]): Convierte un array vacío en una cadena JSON para almacenarlo.
+}
 
 
 const App = () => {
