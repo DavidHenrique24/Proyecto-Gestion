d65d8864ba92c55d75e9f71e9497d410807ab8a2
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-03-11 19:44:27 +0100

    Validaciones

--- src/pages/Registre.jsx
+++ src/pages/Registre.jsx
@@ -17,7 +17,24 @@ const Registro = () => {
     // Obtener lista actual de usuarios
     const usuariosExistentes = obtenerUsuarios();
 
- 
+    // Verificar si el usuario ya está registrado
+    const usuarioExiste = usuariosExistentes.some((usuario) => usuario.email === email); //El método .some() en JavaScript se utiliza para verificar si al menos un elemento en un array cumple con una condición especificada
+    if (usuarioExiste) {
+      setMensaje('Este usuario ya está registrado.');
+      return;
+    }
+
+    // Agregar nuevo usuario
+    const nuevoUsuario = { email, contrasena };
+    const nuevosUsuarios = [...usuariosExistentes, nuevoUsuario];
+
+    // Guardar en localStorage
+    localStorage.setItem('datosUsuarios', JSON.stringify(nuevosUsuarios));
+
+    // Limpiar el formulario y mostrar mensaje de éxito
+    setEmail('');
+    setContrasena('');
+    setMensaje('Registro exitoso.');
   };
 
   return (
@@ -47,7 +64,7 @@ const Registro = () => {
           <input type="submit" className="mt-4 w-100 btn btn-primary" value="Registrarse" />
         </form>
 
-  
+        {mensaje && <p className="text-center mt-3">{mensaje}</p>}
       </div>
     </main>
   );
